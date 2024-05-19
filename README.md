8b51756c23752e2a604ae07e0fdd9eb1224c47f1
Charles Zhang <szinubuntu@gmail.com>
2024-05-20 07:22:05 +0800

    Implement HTTP image loading (jpg only)
    - Modify image loading call architecture (make async)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This demo examines loading image from runtime from different sources:
 1. Local file
-2. Network file
+2. Network file (http/https)
 3. WebSocket binary stream
 4. WebSocket text stream
 
@@ -11,3 +11,5 @@ The image type can be anything that Godot supports (at runtime): png, jpg, svg, 
 ## Reference
 
 Only a single documentation page is needed as Godot supports such dynamic contents very well: https://docs.godotengine.org/en/stable/tutorials/io/runtime_file_loading_and_saving.html
+
+For random static http served image: https://picsum.photos/200/300
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,14 +1,45 @@
 extends Node
 
+# Remark: Due to async requirements, this uses messaging model. Instead of using simple return values and set image texture directly within calls, we rely on internal state variables for holding return value and an explicit call back function to finish the request.
+
+# Request handling state
+var _loaded_image:Image = null
+
 # Routines
-func _load_image(address:String) -> Image:
-	return _load_from_file(address)
-	
-func _load_from_file(file_path:String) -> Image:
-	return Image.load_from_file(file_path)
+func _load_image(address:String):
+	if address.find("http") == 0:
+		_load_from_http(address)
+	else: 
+		_load_from_file(address)
+
+func _load_from_http(address):
+	var request:HTTPRequest = HTTPRequest.new()
+	get_node(".").add_child(request)
+	request.request_completed.connect(_on_request_completed)
+	request.request(address)
+
+func _on_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("Image couldn't be downloaded. Try a different image.")
+
+	var image = Image.new()
+	var error = image.load_jpg_from_buffer(body) # Assuming jpg; This part is tricky - we need to figure out the right file type otherwise this function will cause error
+	if error != OK:
+		push_error("Couldn't load the image.")
+		
+	_loaded_image = image
+	_on_image_loaded()
+
+func _load_from_file(file_path:String):
+	_loaded_image = Image.load_from_file(file_path)
+	_on_image_loaded()
 
 # Signals
 func _on_button_pressed():
 	var address:String = %AddressText.text
-	var image:Image = _load_image(address)
+	_load_image(address)
+
+# Callbacks
+func _on_image_loaded():
+	var image:Image = _loaded_image
 	%TextureRect.texture = ImageTexture.create_from_image(image)
